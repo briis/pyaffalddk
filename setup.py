7ed0918d59eec6d411fab9284e8cf8b716a69619
Bjarne Riis <bjarne@briis.com>
2024-04-25 06:55:42 +0200

    Bump to 2.0.27

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="pyaffalddk",
-    version="2.0.26",
+    version="2.0.27",
     author="briis",
     author_email="bjarne@briis.com",
     description="Gets garbage collection data from danish Municipalities",
